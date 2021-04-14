4da46854e3e70d22a4673c30e0c6f06886997fb2
Simon Hensel <simon.hensel@protonmail.com>
2021-04-14 18:15:49 +0200

    removed main function

--- bulk-rename.py
+++ bulk-rename.py
@@ -2,48 +2,43 @@
 
 import os
 
-def main():
 
-    filepath = r'C:\Users\me\Music'
-    #filepath = r'D:\Music'
+filepath = r'C:\Users\me\Music'
+#filepath = r'D:\Music'
 
-    '''
-    # 'walk' down the directory structure
-    for path, dirs, files in os.walk(filepath):
-        for file in files:
-            
-            # replace all underscores with spaces
-            if '___' in file:
-                new_file = file.replace('___', ' - ')
-            if '---' in file:
-                new_file = file.replace('---', ' - ')
-            if '_' in file:
-                new_file = file.replace('_', ' ')
-            else:
-                new_file = file
-            
-            # replace track numbers with the artist
-            new_file = 'Stand Atlantic' + new_file[2:]
+'''
+# 'walk' down the directory structure
+for path, dirs, files in os.walk(filepath):
+    for file in files:
         
-            os.rename(path + '\\' + file, path + '\\' + new_file)
-    '''
-
-
-    # bulk rename images
-    i = 1
-    for path, dirs, files in os.walk(filepath):
-        for file in files:
-            # check file endings
-            if file[-4:] == '.png':
-                new_file = str(i) + '.png'
-            elif file[-4:] == '.jpg':
-                new_file = str(i) + '.jpg'
-            elif file[-4:] == 'jpeg':
-                new_file = str(i) + '.jpeg'
+        # replace all underscores with spaces
+        if '___' in file:
+            new_file = file.replace('___', ' - ')
+        if '---' in file:
+            new_file = file.replace('---', ' - ')
+        if '_' in file:
+            new_file = file.replace('_', ' ')
+        else:
+            new_file = file
         
-            os.rename(path + '\\' + file, path + '\\' + new_file)
-            i += 1
+        # replace track numbers with the artist
+        new_file = 'Stand Atlantic' + new_file[2:]
+    
+        os.rename(path + '\\' + file, path + '\\' + new_file)
+'''
 
 
-if __name__ == '__main__':
-    main()
+# bulk rename images
+i = 1
+for path, dirs, files in os.walk(filepath):
+    for file in files:
+        # check file endings
+        if file[-4:] == '.png':
+            new_file = str(i) + '.png'
+        elif file[-4:] == '.jpg':
+            new_file = str(i) + '.jpg'
+        elif file[-4:] == 'jpeg':
+            new_file = str(i) + '.jpeg'
+    
+        os.rename(path + '\\' + file, path + '\\' + new_file)
+        i += 1
