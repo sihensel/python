be7b2f0ec6cde8552ced27fd7e077187677940fd
Simon Hensel <simon.hensel@protonmail.com>
2021-12-22 21:47:00 +0100

    Rework spells
    * Change filetype from dos to unix

--- Game/spells.py
+++ Game/spells.py
@@ -2,21 +2,23 @@
 spells that can be casted by the player and NPCs (incliuding enemies)
 '''
 
-class spell:
-    def __init__(self, name, cost, target, effect, amount, duration=0, min_lvl=1):
-        self.name = name
-        self.cost = cost
-        self.target = target
-        self.effect = effect
-        self.amount = amount
-        self.duration = duration
-        self.min_level = min_lvl
+class Spell:
+    def __init__(self, name, mana_cost, target, effect, amount, duration=0, min_lvl=1):
+        self._name = name
+        self._mana_cost = mana_cost
+        self._target = target
+        self._effect = effect
+        self._amount = amount
+        self._duration = duration
+        self._min_level = min_lvl
     
     def get_spell(self):
-        return self.name, self.cost, self.target, self.effect, self.amount, self.duration, self.min_level
+        return {'name': self._name, 'mana_cost': self._mana_cost, 'target': self._target, 'effect': self._effect,
+                'amount': self._amount, 'duraction': self._duration, 'min_level': self._min_level}
 
 ''' SPELL LIST '''
-weak_healing = spell("Small Healing", 20, "self", "healing", 30)
-fireball = spell("Fireball", 30, "enemy", "damage", 35)
-magic_armor = spell("Magic Armor", 40, "self", "buff", 30, 5, 3)
-weaken = spell("Weaken", 40, "enemy", "debuff", 10, 5, 3)
+# TODO: Effects need to be reworked
+weak_healing = Spell("Small Healing", 20, "self", "healing", 30)
+fireball = Spell("Fireball", 30, "other", "damage", 35)
+magic_armor = Spell("Magic Armor", 40, "self", "buff", 30, 5, 3)
+weaken = Spell("Weaken", 40, "other", "debuff", 10, 5, 3)
