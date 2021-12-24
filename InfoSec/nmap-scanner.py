1fe232ba76e5d9389fcd33627d4092ea6bbcfb6a
Simon Hensel <simon.hensel@protonmail.com>
2021-12-24 16:29:14 +0100

    Update and improve script
    * Expand user input options
    * Implement Error handling
    * Linting

--- InfoSec/nmap-scanner.py
+++ InfoSec/nmap-scanner.py
@@ -1,65 +1,84 @@
 #!/usr/bin/env python3
 
 '''
-A small script that executes different nmap scans
-
-Part of this is still broken and the output needs to be formated properly
+A small script that executes different nmap scans depending on user input
+https://github.com/sihensel
 '''
 
-import time, sys
+import time
+import sys
 
 try:
     import nmap
 except ImportError:
     print('Missing dependency "python-nmap", exiting.')
-    sys.exit()
+    sys.exit(1)
 
-scanner = nmap.PortScanner()
+nm = nmap.PortScanner()
 
 print('--- Small Python nmap automation script ---')
 
-ip_addr = input('Please enter the IP to scan: ')
-#ip_addr = '192.168.1.1'
-ports = '1-1024'    # maybe make this interactive in the future
+while True:
+    # wait until input is valid or user quits
+    cmd = input('''\nPlease enter the type of scan (q to quit):
+                   1) TCP Scan (-sS)
+                   2) UDP Scan (-sU)
+                   3) Comprehensive Scan (-sS -sV -sC -A -O)\n''')
 
+    # exit on q or Q
+    if cmd == 'q' or cmd == 'Q':
+        sys.exit(0)
+    elif cmd in ['1', '2', '3']:
+        break
 
-cmd = input('''\nPlease enter the type of scan:
-               1) TCP Scan (-sS)
-               2) UDP Scan (-sU)
-               3) Comprehensive Scan (-sS -sV -sC -A -O)\n''')
+ip_addr = input('Enter the IP to scan (hostnames don\'t work): ')
 
-print('Starting scan...')
-start_time = time.time()    # measure time
+while True:
+    # wait until upper port limit input is valid
+    try:
+        port_limit = int(input('Enter the upper port limit (default 1024): ')
+                         or 1024)
+    except ValueError:
+        print('The port number has to be an integer')
+    else:
+        break
+
+# generate port range as a string, e.g. '1-1024'
+port_range = f'1-{port_limit}'
+
+print(f'\nStarting scan on host {ip_addr} ...')
+start_time = time.time()
 
 if cmd == '1':
     protocol = 'tcp'
-    scanner.scan(ip_addr, ports, '-v -sS')
-
+    options = '-v -sS'
 elif cmd == '2':
     protocol = 'udp'
-    scanner.scan(ip_addr, ports, '-v -sU')
-
+    options = '-v -sU'
 elif cmd == '3':
     protocol = 'tcp'
-    scanner.scan(ip_addr, ports, '-v -sS -sV -sC -A -O')
+    options = '-v -sS -sV -sC -A -O'
 
-else:
-    print('Please enter a number between 1 and 3.')
-    sys.exit()
+nm.scan(ip_addr, port_range, options)
 
+# aggregate scan results and output them
+print(f'\nNmap version: {nm.nmap_version()[0]}.{nm.nmap_version()[1]}')
 
-print('\nNmap version:', str(scanner.nmap_version()[0]) + '.' + str(scanner.nmap_version()[1]))
-print('IP range:' + 4* ' ', scanner.scaninfo()[protocol]['services'])
-print('IP Status:' + 3* ' ', scanner[ip_addr].state())
+try:
+    print('IP status:' + 3 * ' ', nm[ip_addr].state())
+except KeyError:
+    print(f'Host {ip_addr} is not reachable')
+    sys.exit(1)
 
-print('Open Ports:' + 3* ' ' , end='') 
-for i, j in enumerate(scanner[ip_addr][protocol]):
-    if i < len(scanner[ip_addr][protocol].keys()) -1:
-        print(j, end=', ')
+print('IP range:' + 4 * ' ', nm.scaninfo()[protocol]['services'])
+
+print('Open ports:' + 3 * ' ', end='')
+for index, item in enumerate(nm[ip_addr][protocol].items(), start=1):
+    if item[1]['state'] == 'closed':
+        continue
+    if index == len(nm[ip_addr][protocol].keys()):
+        print(item[0])
     else:
-        print(j)    # dont print a comma after the last item
+        print(item[0], end=', ')
 
 print(f'\nFinished in {round(time.time() - start_time, 2)} seconds.')
-    
-#print(scanner.scaninfo())
-#print(scanner[ip_addr])
