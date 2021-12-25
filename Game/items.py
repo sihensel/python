28042ea3969f12576e620899f5e5189659d296e4
Simon Hensel <simon.hensel@protonmail.com>
2021-12-25 21:11:00 +0100

    Remove is_usable property, add Potion duration
    * Linting

--- Game/items.py
+++ Game/items.py
@@ -8,18 +8,15 @@ All used items will be imported from here
 
 
 class Item:
-    def __init__(self, name, weight,
-                 value=0, equippable=False, usable=False, magic=False):
+    def __init__(self, name, weight, value=0, equippable=False):
         self._name = name
         self._weight = weight
         self._value = value
         self._is_equippable = equippable
-        self._is_usable = usable
 
     def get_item(self):
         return {'name': self._name, 'weight': self._weight,
-                'value': self._value, 'is_equippable': self._is_equippable,
-                'is_usable': self._is_usable}
+                'value': self._value, 'is_equippable': self._is_equippable}
 
 
 class Weapon(Item):
@@ -31,8 +28,7 @@ class Weapon(Item):
     def get_item(self):
         return {'name': self._name, 'weight': self._weight,
                 'value': self._value, 'is_equippable': self._is_equippable,
-                'is_usable': self._is_usable, 'damage': self._damage,
-                'type': self._type}
+                'damage': self._damage, 'type': self._type}
 
 
 class Armor(Item):
@@ -44,21 +40,24 @@ class Armor(Item):
     def get_item(self):
         return {'name': self._name, 'weight': self._weight,
                 'value': self._value, 'is_equippable': self._is_equippable,
-                'is_usable': self._is_usable, 'armor': self._armor,
-                'type': self._type}
+                'armor': self._armor, 'type': self._type}
 
 
 class Potion(Item):
-    def __init__(self, name, weight, value, amount, _type):
-        super().__init__(name, weight, value, usable=True)
+    '''
+    Potions are the only usable items
+    '''
+    def __init__(self, name, weight, value, amount, _type, duration=0):
+        super().__init__(name, weight, value)
         self._amount = amount
         self._type = _type
+        self._duration = duration
 
-    def get_potion(self):
+    def get_item(self):
         return {'name': self._name, 'weight': self._weight,
                 'value': self._value, 'is_equippable': self._is_equippable,
-                'is_usable': self._is_usable, 'amount': self._amount,
-                'type': self._type}
+                'amount': self._amount, 'type': self._type,
+                'duration': self._duration}
 
 
 ''' ITEM LIST '''
@@ -80,8 +79,7 @@ small_health_potion = Potion("Small Health Potion", 0.5, 50, 30, "health")
 great_health_potion = Potion("Great Health Potion", 0.5, 150, 80, "health")
 small_mana_potion = Potion("Small Mana Potion", 0.5, 60, 20, "mana")
 great_mana_potion = Potion("Great Health Potion", 0.5, 170, 45, "mana")
-battle_potion = Potion("Battle Potion", 0.5, 150, 5, "buff")
-poison = Potion("Poison", 0.5, 100, 20, "poison")
+battle_potion = Potion("Battle Potion", 0.5, 150, 5, "buff", 3)
 
 '''MISC'''
 iron_ore = Item("Iron Ore", 2, 5)
