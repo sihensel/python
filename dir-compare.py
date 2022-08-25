c30fd518c6acb11c8c910dcf445e3f780261d9a6
Simon Hensel <simon.hensel@protonmail.com>
2022-08-25 23:32:10 +0200

    Ignore file extensions when comparing directories

--- dir-compare.py
+++ dir-compare.py
@@ -2,13 +2,13 @@
 
 '''
 Small utility to check if two directories contain the same files
-This can be usefull when manually creating backups
+Can be usefull when manually creating backups
 '''
 
 import os
 
-source_dir = '/home/simon/studium'
-target_dir = '/home/simon/HDD/Studium'
+source_dir = '/home/simon/music'
+target_dir = '/home/simon/HDD/Music'
 
 # When using Windows
 source_dir = r'D:\Music'
@@ -17,15 +17,15 @@ target_dir = r'E:\Music'
 source_dir_files = []
 target_dir_files = []
 
-# get all files from source_dir
+# get all files from source_dir, ignoring file extensions
 for _, _, files in os.walk(source_dir):
     for file in files:
-        source_dir_files.append(file)
+        source_dir_files.append(file.split('.')[0])
 
-# get all files from target_dir
+# get all files from target_dir, ignoring file extensions
 for _, _, files in os.walk(target_dir):
     for file in files:
-        target_dir_files.append(file)
+        target_dir_files.append(file.split('.')[0])
 
 print(f'The following files are NOT in the target dir {target_dir}\n')
 # check if the file exists in target_dir
